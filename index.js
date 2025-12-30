3af31fede5595fbd58c079d3cfebc4bdff54a5a1
Julien Bouquillon <julien@bouquillon.com>
2025-12-30 16:12:08 +0100

    almost

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const port = 8080;
 
 app.get("/", (req, res) => {
   let html = "<h1>hello from express</h1>";
-  html += `<a href='/oauth2/sign_out?rd=https://${req.headers["x-forwarded-host"]}'>Logout</a> | <a href='/oauth2/userinfo'>userinfo</a><br/><br/>\n`;
+  html += `<a href='/oauth2/sign_out'>Logout</a> | <a href='/oauth2/userinfo'>userinfo</a><br/><br/>\n`;
   html += `<li>Logged in as ${req.headers["x-forwarded-user"]}</li>\n`;
   html += `<li>Email: ${req.headers["x-forwarded-email"]}</li>\n`;
   html += `<li>Real IP: ${req.headers["x-real-ip"]}</li>\n`;
-  //html += `<br/><br/>${JSON.stringify(req.headers)}`;
+  html += `<br/><br/>${JSON.stringify(req.headers)}`;
   html +=
     "<br/><br/><a href='https://github.com/betagouv/oauth2-deploy-demo'>Demo repo</a>";
   res.send(html);
